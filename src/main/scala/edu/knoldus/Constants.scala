20677b34117f26e8a3bd0cef0b7a67aebdbcde9e
Neelam Lakra <neelam@knoldus.in>
2018-01-17 17:25:40 +0530

    .idea removed

--- src/main/scala/edu/knoldus/Constants.scala
+++ src/main/scala/edu/knoldus/Constants.scala
@@ -1,9 +1,13 @@
 package edu.knoldus
 
-class Constants {
+object Constants {
 
   //scalastyle:off
-  val FIFTY = 50
+  val FIFTYONE = 51
+  val ZERO = 0
+  val THREE = 3
+  val ONE = 1
+  val SIX = 6
 
   //scalastyle:on
 
--- src/main/scala/edu/knoldus/PersonClassification.scala
+++ src/main/scala/edu/knoldus/PersonClassification.scala
@@ -4,44 +4,55 @@ import org.apache.log4j.Logger
 import scala.util.Random
 
 abstract class PersonClassification
+
 case class Gamer() extends PersonClassification
+
 case class Trainer() extends PersonClassification
-case class Blogger() extends PersonClassification
+
+case class Blogger(blogList: Map[String, Int], numberOfBlogs: Int) extends PersonClassification
 
 object Operation extends PersonClassification with App {
+
   val log = Logger.getLogger(this.getClass)
 
   def response(person: PersonClassification): String = {
     person match {
       case g: Gamer => gamer()
-      case t: Trainer =>trainer()
-      //case b: Blogger =>
-      case _=>"no such person exist"
+      case t: Trainer => trainer()
+      //case b: Blogger =>blogger()
+      case _ => "no such person exist"
     }
   }
 
   def gamer(): String = {
     def diceCount(count: Int): String = {
-      val ran = scala.util.Random
-      val diceNum = 1 + ran.nextInt(6)
-
-      diceNum match {
-
-        case 1 | 6 if (count != 0) => diceCount(count - 1)
-        case 1 | 6 if (count == 0) => "person is winner \n "
-        case _ => "person is looser \n"
+      if (count > 0 && count <= 3) {
+        val randomNumber = scala.util.Random
+        val diceNum = Constants.ZERO + randomNumber.nextInt(Constants.SIX)
+        diceNum match {
+          case Constants.ONE | Constants.SIX => if (count == 0) "person is winner \n" else diceCount(count - 1)
+          case _ => "person is looser \n"
+        }
+      }
+      else {
+        " please roll the dice atleast once \n"
       }
     }
-    diceCount(2)
+
+    diceCount(0)
   }
 
-  def trainer(): String ={
-      val strength = scala.util.Random
-     s" attendance is: ${strength.nextInt(50)}"
-    }
+  def trainer(): String = {
+    val strength = scala.util.Random
+    s" attendance is: ${strength.nextInt(Constants.FIFTYONE)}"
+  }
 
-  def blogger()
+  /*def blogger(blogList: Map[String, Int], numberOfBlogs: Int) :String = {
+
+  }*/
 
   log.info(response(Gamer()))
   log.info(response(Trainer()))
+
 }
+
